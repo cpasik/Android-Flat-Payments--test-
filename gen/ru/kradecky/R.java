81c617aa211b76ce4ef1dc84e129214c7e7a79c5
George <cpasik@gmail.com>
2011-06-17 21:33:14 +0400

    added OK button to new tariff form

--- gen/ru/kradecky/R.java
+++ gen/ru/kradecky/R.java
@@ -14,10 +14,11 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
+        public static final int btnOK=0x7f050001;
         public static final int btnTariffs=0x7f050000;
-        public static final int btnTariffsAdd=0x7f050002;
-        public static final int tariffWelcome=0x7f050001;
-        public static final int text=0x7f050003;
+        public static final int btnTariffsAdd=0x7f050003;
+        public static final int tariffWelcome=0x7f050002;
+        public static final int text=0x7f050004;
     }
     public static final class layout {
         public static final int main=0x7f030000;
@@ -26,6 +27,8 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int btnCancel=0x7f040008;
+        public static final int btnOK=0x7f040007;
         public static final int btnTariffs=0x7f040003;
         public static final int btnTariffsAdd=0x7f040004;
         public static final int hello=0x7f040000;
