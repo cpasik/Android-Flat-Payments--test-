d4baefe876a9fa2b13324b93b5740ee30dbd768b
George <cpasik@gmail.com>
2011-06-22 06:22:46 +0400

    fixed dialog width & height

--- src/ru/kradecky/flat_payments/NewCounter.java
+++ src/ru/kradecky/flat_payments/NewCounter.java
@@ -4,19 +4,26 @@ import ru.kradecky.flat_payments.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.WindowManager.LayoutParams;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class NewCounter extends Activity implements OnItemSelectedListener
+public class NewCounter extends Activity implements OnItemSelectedListener, OnClickListener
 {
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.new_counter);
+		
+		LayoutParams params = getWindow().getAttributes();
+		params.height = LayoutParams.FILL_PARENT;
+		params.width  = LayoutParams.FILL_PARENT;
+		getWindow().setAttributes((android.view.WindowManager.LayoutParams)params);
 		
 		Spinner spinner = (Spinner) findViewById(R.id.counter_type);
 		ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.counter_array, android.R.layout.simple_spinner_item);
@@ -36,4 +43,12 @@ public class NewCounter extends Activity implements OnItemSelectedListener
 		
 	}
 
+	public void onClick(View item) 
+	{
+		switch (item.getId())
+		{
+			case R.id.btnOK:
+				break;
+		}
+	}
 }
